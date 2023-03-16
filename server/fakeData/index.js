7e1f51b301d7204e3c3c65e6018f0850121f5d86
Huy Quoc <proimpossible0809@gmail.com>
2023-03-16 15:42:55 +0700

    first commit

---
+++ server/fakeData/index.js
@@ -0,0 +1,33 @@
+export default{
+    authors:[
+        {
+            id:1,
+            name:'Quoc'
+        },
+        {
+            id:2,
+            name:'Quoc2'
+        }
+    ],
+    folders: [
+        {
+            id:1,
+            name:'Home',
+            createdAt:'2022-11-18T03L:42:13',
+            authorId:1,
+        },
+        {
+            id:2,
+            name:'Home2',
+            createdAt:'2022-11-18T03L:42:13',
+            authorId:1,
+        },
+        {
+            id:3,
+            name:'Home3',
+            createdAt:'2022-11-18T03L:42:13',
+            authorId:2,
+        },
+
+    ]
+}
